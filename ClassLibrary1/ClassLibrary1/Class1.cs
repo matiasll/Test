278928b7bffda337ba405443ecbc81ebf20dc5eb
matiasll <hector.llampa@gmail.com>
2013-03-16 03:28:38 -0300

    "numeros romanos"

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int convertir(string romano)
+        {
+            var vecRomano = romano.ToCharArray();
+            int contador = 0;
+            char primero = vecRomano[0];
+            int ant = 0;
+            for (int i = 0; i <= (vecRomano.Length - 1); i++)
+            {
+                if (this.controlarMayor(vecRomano[i], vecRomano[ant]))
+                {
+                    contador = contador + this.dameTuValor(vecRomano[i]);
+                }
+                else
+                {
+                    contador = this.dameTuValor(vecRomano[i]) - contador;
+                }
+
+                if (i != 0)
+                    ant++;
+            }
+
+            return contador;
+
+        }
+
+        private bool controlarMayor(char pos, char ant)
+        {
+            return this.dameTuValor(pos) <= this.dameTuValor(ant);
+        }
+
+        private int dameTuValor(char pos)
+        {
+            if (pos.Equals('I'))
+                return 1;
+
+            if (pos.Equals('V'))
+                return 5;
+
+            if (pos.Equals('X'))
+                return 10;
+
+            if (pos.Equals('L'))
+                return 50;
+
+            if (pos.Equals('C'))
+                return 100;
+
+            if (pos.Equals('D'))
+                return 500;
+
+            if (pos.Equals('M'))
+                return 1000;
+            return 0;
+        }
+        public bool validar(string h)
+        {
+            var vecCaract = h.ToCharArray();
+            Stack<char> pilaAbierta = new Stack<char>();
+            for (int i = 0; i <= (vecCaract.Length - 1); i++)
+            {
+                if (abre(vecCaract[i]))
+                {
+                    pilaAbierta.Push(vecCaract[i]);
+                }
+
+                if (cierra(vecCaract[i]))
+                    if (!(this.comparar(pilaAbierta.Pop, vecCaract[i])))
+                    {
+                        return false;
+                    }
+
+            }
+            return true;
+        }
+
+        private bool cierra(char p)
+        {
+            return p.Equals(']') || p.Equals(')') || p.Equals('}');
+
+        }
+
+
+
+        private bool abre(char p)
+        {
+            return p.Equals('{') || p.Equals('(') || p.Equals('[');
+        }
+
+    }
+}
---
+++ ClassLibrary1/TestProject1/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConvert1()
+        {
+            string a = "I";
+            var clase = new Class1();
+            int resultado = clase.convertir(a);
+            Assert.IsTrue(resultado.Equals(1));
+        }
+        [TestMethod]
+        public void TestConvert2()
+        {
+            string a = "II";
+            var clase = new Class1();
+            int resultado = clase.convertir(a);
+            Assert.IsTrue(resultado.Equals(2));
+        }
+        [TestMethod]
+        public void TestConvert3()
+        {
+            string a = "III";
+            var clase = new Class1();
+            int resultado = clase.convertir(a);
+            Assert.IsTrue(resultado.Equals(3));
+        }
+        [TestMethod]
+        public void TestConvert4()
+        {
+            string a = "IV";
+            var clase = new Class1();
+            int resultado = clase.convertir(a);
+            Assert.IsTrue(resultado.Equals(4));
+        }
+        [TestMethod]
+        public void TestConvert5()
+        {
+            string a = "CM";
+            var clase = new Class1();
+            int resultado = clase.convertir(a);
+            Assert.IsTrue(resultado.Equals(900));
+        }
+        [TestMethod]
+        public void TestConvert6()
+        {
+            string a = "(fgdgd){}";
+            var clase = new Class1();
+            bool resultado = clase.validar(a);
+            Assert.IsTrue(resultado);
+        }
+        
+
+  }
+}
